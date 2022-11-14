faba104425ff0ebe0f3978e9462e9dc84fba403c
gaeun <knospe1@naver.com>
2022-11-14 10:45:04 +0900

    Add Provider

--- lib/model/ui/home_screen.dart
+++ lib/model/ui/home_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:image_search/data/pixabay_api.dart';
 import 'package:image_search/data/photo_provider.dart';
 import 'package:image_search/model/photo.dart';
+import 'package:image_search/model/ui/home_view_model.dart';
 import 'package:image_search/model/ui/widget/photo_widget.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -24,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final viewModel = PhotoProvider.of(context).viewModel;
+    final viewModel = context.watch<HomeViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text(
